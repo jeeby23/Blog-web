aa284d21ed239c4b310e871ce0b7ed8d8ec8fe87
mujeeb <shola.mujeeb@gmail.com>
2026-06-03 18:13:57 -0700

    fix: cloudinary install issue

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,18 +1,22 @@
-import nodemailer from "nodemailer"
+import nodemailer from 'nodemailer'
 
 const sendEmail = async (to, subject, text) => {
-    const transporter = nodemailer.createTransport({
-        service:"gmail",
-        auth:{
-            user:process.env.EMAIL,
-            pass:process.env.EMAIL_PASS
-        },
-    })
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASS,
+    },
+  })
+  try {
     await transporter.sendMail({
-        from:process.env.EMAIL,
-        to,
-        subject,
-        text,
+      from: process.env.EMAIL,
+      to,
+      subject, 
+      text,
     })
+  } catch (error) {
+    console.error('Email error:', error)
+  }
 }
 export default sendEmail
