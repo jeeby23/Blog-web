9567cd20028e0db35ea065b8e69bb00df71738bf
mujeeb <shola.mujeeb@gmail.com>
2026-06-04 06:03:04 -0700

    update pack

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ import swaggerUi from "swagger-ui-express"
 import swaggerSpec from "../config/swagger.js"
 dotenv.config()
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const app = express()
 app.use(cookieParser())
 app.use(express.json())
